1a4fa203ae29febe75ddacd6a4906f9d685cd673
Luis Pomare <luispomare92@gmail.com>
2022-09-16 10:10:20 -0500

    Create APP components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TopBarComponent, ProductListComponent, ProductAlertsComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
