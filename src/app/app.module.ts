2476d01fbc64ff051d3fd9610f0007952ace88f5
Luis Pomare <luispomare92@gmail.com>
2022-09-16 10:03:31 -0500

    Configure APP root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { DivComponent } from './components/div/div.component';
-
 @NgModule({
-  declarations: [AppComponent, DivComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
